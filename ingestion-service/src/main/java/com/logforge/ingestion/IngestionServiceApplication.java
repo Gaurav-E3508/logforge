fe94fa050dfe5e5c3972fb553c6afbace88c86d3
Gaurav Erande <gauraverande05@gmail.com>
2026-06-19 16:36:17 +0530

    fixed .yml file

--- ingestion-service/src/main/java/com/logforge/ingestion/IngestionServiceApplication.java
+++ ingestion-service/src/main/java/com/logforge/ingestion/IngestionServiceApplication.java
@@ -3,6 +3,8 @@ package com.logforge.ingestion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class IngestionServiceApplication {
     public static void main(String[] args) {
